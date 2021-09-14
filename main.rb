0de83bd66fa4c992fada7f9d8fe6687bc1f6a60a
Raymond <raymond@philippinedev.com>
2021-09-14 16:34:46 +0800

    command line executable main.rb

---
+++ main.rb
@@ -0,0 +1,22 @@
+#!/usr/bin/env ruby
+
+require 'pry'
+
+Dir["./classes/command/*.rb"].each { |file| require file }
+
+def main
+  unless ARGV.count == 1
+    return "ERROR: Can only process one file at a time."
+  end
+
+  file_path = ARGV[0]
+
+  unless File.exist? file_path
+    return "ERROR: File does not exist."
+  end
+
+  Processor.call(file_path)
+end
+
+puts main
+
