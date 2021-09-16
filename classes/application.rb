8d2acbbab08364a0976cdac795f6c9db045fde63
Raymond <raymond@philippinedev.com>
2021-09-16 15:08:28 +0800

    refactor

--- classes/application.rb
+++ classes/application.rb
@@ -1,25 +1,6 @@
 class Application
   class << self
     def start!
-      new
-      Seed.call
-    end
-
-    def database
-      @@database
-    end
-
-    def push(person)
-      @@database << person
-    end
-
-    def find_by_name(name)
-      @@database.filter { |row| row.name == name }
-        .first
-    end
-
-    def reseed
-      @@database = []
       Seed.call
     end
 
@@ -29,10 +10,6 @@ class Application
         AddChildTransformer
       ]
     end
-  end
-
-  def initialize
-    @@database = []
   end
 end
 
